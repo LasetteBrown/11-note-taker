59342773f31f0eaba8e3cf62a8241b6bfffcdd83
Lasette Brown <lasette.brown@gmail.com>
2021-09-26 11:30:47 -0600

    set up server.

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const path = require("path");
 
+//sets up the server
 const app = express();
 const PORT = 3001;
 
 app.use(express.static("public"));
 
+//GET route for the homepage
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
+//GET route for the notes page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
+// Wildcard route to direct users to a 404 page
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/pages/404.html"))
+);
+
+//listens at local host port
 app.listen(PORT, () =>
   console.log(`app listening at http://localhost:${PORT}`)
 );
